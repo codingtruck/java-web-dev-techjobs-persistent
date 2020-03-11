0fcca14f6ac720f82793a0a58e71a31291fc02bb
Travon <andrewarren113@gmail.com>
2020-03-11 18:49:46 -0500

    First commit final assignment

--- queries.sql
+++ queries.sql
@@ -1,6 +1,13 @@
 ## Part 1: Test it with SQL
 
+column = id, datatype = int: column = employer, datatype = varchar(255):
+column = name, datatype = varchar(255): column = skills, datatype = varchar(255)
+
 ## Part 2: Test it with SQL
+
+select *
+from employer
+where location = "Saint Louis City";
 
 ## Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
+    @Valid
+    private String location;
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
